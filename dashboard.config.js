5e40cfa7e95e531c1110a634a72c54c13fdafcee
Dark-Law <kkulshi12@gmail.com>
2025-11-28 21:27:23 +0300

    Update dashboard.config.js

--- dashboard.config.js
+++ dashboard.config.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    owners: ["", ""],
-    clientID: "",
-    clientSECRET: "",
+    owners: ["1764344206105", "1764344466411"],
+    clientID: "1444006544797859882",
+    clientSECRET: "4fa209fa76c20cb10eb187c20910da5033891c416c51325a1433226877b5a783",
     domain: "http://localhost:3030",
     port: 3030
 };
+
